12e22d58f996647b4b342f1a5f79295459621046
eykd <david.eyk@gmail.com>
2015-05-12 22:51:35 -0700

    Vendor'd

--- gulp/config.js
+++ gulp/config.js
@@ -47,17 +47,6 @@ module.exports = {
       outputName: 'main.js',
       // Additional file extentions to make optional
       extensions: ['.coffee', '.hbs'],
-      // list of modules to make require-able externally
-      require: ['jquery', 'backbone/node_modules/underscore']
-      // See https://github.com/greypants/gulp-starter/issues/87 for note about
-      // why this is 'backbone/node_modules/underscore' and not 'underscore'
-    }, {
-      entries: src + '/scripts/game.coffee',
-      dest: dest,
-      outputName: 'game.js',
-      extensions: ['.coffee', '.hbs'],
-      // list of externally available modules to exclude from the bundle
-      external: ['jquery', 'underscore']
     }]
   },
   production: {
